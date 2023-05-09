1c46da3ca180490f251053f70d7e2c9ebc7c1f5f
attila <attila.lundin@gmail.com>
2023-05-09 16:47:06 +0200

    Changes by Odai

--- src/sharedresources/requests/GetChatRoomsRequest.java
+++ src/sharedresources/requests/GetChatRoomsRequest.java
@@ -1,4 +1,20 @@
 package sharedresources.requests;
 
-public class GetChatRoomsRequest {
+import server.ChatRoomStorage;
+import server.UserStorage;
+import sharedresources.ChatRoom;
+import sharedresources.interfaces.DataHandler;
+
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class GetChatRoomsRequest implements DataHandler, Serializable {
+
+    private ChatRoom chatRoom;
+    public void GetChatRoomRequest(ChatRoom chatRoom){
+        this.chatRoom = chatRoom;
+    }
+    public void dataHandler(UserStorage userStorage, ChatRoomStorage chatroomStorage, ObjectOutputStream outputStream) {
+
+    }
 }
